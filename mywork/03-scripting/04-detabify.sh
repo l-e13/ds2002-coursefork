6440ea0dc33ab74f2bd667c54ab0b32351e24178
Lily England <lilyengland@Lilys-MacBook-Air.local>
2024-02-10 17:30:43 -0500

    adding detabify script

--- mywork/03-scripting/04-detabify.sh
+++ mywork/03-scripting/04-detabify.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+set -e
+
+if [ -z "$1" ]; then
+  echo "This script converts a TSV into a CSV";
+  echo "Append the input file name and then the output file name";
+fi
+
+# This will turn TSV into CSV
+
+/usr/bin/tr '\t' ',' < $1 > $2
