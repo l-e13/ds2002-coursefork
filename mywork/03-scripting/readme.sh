0a380ef27ebd10c7192c5c1b23e1ad4bd0dc4f5e
Lily England <lilyengland@Lilys-MacBook-Air.local>
2024-02-13 16:14:15 -0500

    readme

---
+++ mywork/03-scripting/readme.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script is designed to ________ 
+# Author: Lily England <jww2fj@virginia.edu>
+# Last updated: 2/6/24
+
+read -p "What is your favorite number?" FAVNUM
+echo "Hey $USER I heard your favorite number is $FAVNUM"
+
